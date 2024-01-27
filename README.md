92eea64ae2c0e72f347ab967f00fbfde8f8a3a39
Sarthak Negi <srnegi141@gmail.com>
2024-01-26 19:58:06 -0600

    changed formatting configs

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 
 ## To run the backend server -
 
-- `cd backend`
-- `python -m venv menv`
-- `./menv/Scripts/activate`
-- Select python interpreter to be venv
-- `uvicorn app:app`
+-   `cd backend`
+-   `python -m venv menv`
+-   `./menv/Scripts/activate`
+-   Select python interpreter to be venv
+-   `uvicorn app:app`
 
 ## To run the frontend -
 
-- `cd frontend`
-- `npm install`
-- `npm run dev`
+-   `cd frontend`
+-   `npm install`
+-   `npm run dev`
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,30 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
-  const [data, setData] = useState({});
+export default function App() {
+    const [data, setData] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://127.0.0.1:8000/");
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://127.0.0.1:8000/");
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
 
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error("Error fetching data:", error.message);
-      }
-    };
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                console.error("Error fetching data:", error.message);
+            }
+        };
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  return (
-    <div>
-      <h1>{data.message}</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{data.message}</h1>
+        </div>
+    );
 }
-
-export default App;
