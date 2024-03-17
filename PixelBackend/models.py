d5859310fd680607e66175dc329388bdd9c4c086
Sarthak <srnegi141@gmail.com>
2024-03-16 20:27:20 -0500

    renamed backend dir

---
+++ PixelBackend/models.py
@@ -0,0 +1,8 @@
+from .database import Base
+from sqlalchemy import Column, String
+
+class User(Base):
+    __tablename__ = "Users"
+
+    email = Column(String, unique=True, index=True, primary_key=True, nullable=False)
+    password = Column(String, nullable=False)
