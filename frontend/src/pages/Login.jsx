a9d845360af5dfc9bef2530be9867519723e9e22
Sarthak Negi <srnegi141@gmail.com>
2024-03-18 22:14:45 -0500

    login page designed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,59 @@
 import { InputText } from "primereact/inputtext";
+import { Formik, Form, ErrorMessage } from "formik";
+import { Button } from "primereact/button";
+import "../assets/css/login.css";
+import { useState } from "react";
 
 const Login = () => {
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleSubmit = (credentials) => {
+        setCredentials(credentials);
+        console.log(credentials);
+    };
     return (
         <div className="loginContainer">
-            <h2>Login</h2>
-            <div className="p-fluid">
-                <div className="p-field">
-                    <label htmlFor="email">Email</label>
-                    <InputText id="email" type="email" placeholder="email" />
-                </div>
-                <div className="p-field">
-                    <label htmlFor="password">Password</label>
-                    <InputText id="password" type="password" />
-                </div>
-            </div>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                onSubmit={handleSubmit}
+            >
+                {() => (
+                    <Form>
+                        <div className="formField">
+                            <label htmlFor="email">Email</label>
+                            <InputText id="email" type="email" name="email" />
+                            <ErrorMessage
+                                name="email"
+                                component="div"
+                                className="error"
+                            />
+                        </div>
+                        <div className="formField">
+                            <label htmlFor="password">Password</label>
+                            <InputText
+                                id="password"
+                                type="password"
+                                name="password"
+                            />
+                            <ErrorMessage
+                                name="password"
+                                component="div"
+                                className="error"
+                            />
+                        </div>
+                        <div className="buttons">
+                            <Button type="submit" label="Login" />
+                            <Button type="button" label="Register" />
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 };
