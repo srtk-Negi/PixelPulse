3350b8f5c4df55140c0a77f349b208364f02382a
Sarthak <srnegi141@gmail.com>
2024-03-23 00:05:53 -0500

    updated the color schemes

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
 import { Formik, Form, ErrorMessage } from "formik";
 import { Button } from "primereact/button";
 import { registrationSchema } from "../schemas";
@@ -41,145 +42,156 @@ const RegisterPage = () => {
                 >
                     {({ values, handleSubmit, handleChange }) => (
                         <Form onSubmit={handleSubmit}>
-                            {/* first_name*/}
-                            <div className="formField">
-                                <label htmlFor="first_name">First Name</label>
-                                <InputText
-                                    id="first_name"
-                                    type="text"
-                                    name="first_name"
-                                    values={values.first_name}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="first_name"
-                                    render={(msg) => (
-                                        <small
-                                            id="title-help"
-                                            className="p-error"
-                                        >
-                                            {msg}
-                                        </small>
-                                    )}
-                                />
+                            <div className="formFieldGroup">
+                                {/* first_name*/}
+                                <div className="formField">
+                                    <label htmlFor="first_name">
+                                        First Name
+                                    </label>
+                                    <InputText
+                                        id="first_name"
+                                        type="text"
+                                        name="first_name"
+                                        values={values.first_name}
+                                        onChange={handleChange}
+                                        placeholder="John..."
+                                    />
+                                    <ErrorMessage
+                                        name="first_name"
+                                        render={(msg) => (
+                                            <small
+                                                id="title-help"
+                                                className="p-error"
+                                            >
+                                                {msg}
+                                            </small>
+                                        )}
+                                    />
+                                </div>
+                                {/* last_name*/}
+                                <div className="formField">
+                                    <label htmlFor="last_name">Last Name</label>
+                                    <InputText
+                                        id="last_name"
+                                        type="text"
+                                        name="last_name"
+                                        values={values.last_name}
+                                        onChange={handleChange}
+                                        placeholder="Doe..."
+                                    />
+                                    <ErrorMessage
+                                        name="last_name"
+                                        render={(msg) => (
+                                            <small
+                                                id="title-help"
+                                                className="p-error"
+                                            >
+                                                {msg}
+                                            </small>
+                                        )}
+                                    />
+                                </div>
                             </div>
-                            {/* last_name*/}
-                            <div className="formField">
-                                <label htmlFor="last_name">Last Name</label>
-                                <InputText
-                                    id="last_name"
-                                    type="text"
-                                    name="last_name"
-                                    values={values.last_name}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="last_name"
-                                    render={(msg) => (
-                                        <small
-                                            id="title-help"
-                                            className="p-error"
-                                        >
-                                            {msg}
-                                        </small>
-                                    )}
-                                />
+                            <div className="formFieldGroup">
+                                {/* email*/}
+                                <div className="formField">
+                                    <label htmlFor="email">Email</label>
+                                    <InputText
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        values={values.email}
+                                        onChange={handleChange}
+                                        placeholder="someone@abc.com"
+                                    />
+                                    <ErrorMessage
+                                        name="email"
+                                        render={(msg) => (
+                                            <small
+                                                id="title-help"
+                                                className="p-error"
+                                            >
+                                                {msg}
+                                            </small>
+                                        )}
+                                    />
+                                </div>
+                                {/* Phone Number*/}
+                                <div className="formField">
+                                    <label htmlFor="phone">Phone</label>
+                                    <InputText
+                                        id="phone"
+                                        type="text"
+                                        name="phone"
+                                        values={values.phone}
+                                        onChange={handleChange}
+                                        placeholder="123-456-7890"
+                                    />
+                                    <ErrorMessage
+                                        name="phone"
+                                        render={(msg) => (
+                                            <small
+                                                id="title-help"
+                                                className="p-error"
+                                            >
+                                                {msg}
+                                            </small>
+                                        )}
+                                    />
+                                </div>
                             </div>
-                            {/* email*/}
-                            <div className="formField">
-                                <label htmlFor="email">Email</label>
-                                <InputText
-                                    id="email"
-                                    type="email"
-                                    name="email"
-                                    values={values.email}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="email"
-                                    render={(msg) => (
-                                        <small
-                                            id="title-help"
-                                            className="p-error"
-                                        >
-                                            {msg}
-                                        </small>
-                                    )}
-                                />
+                            <div className="formFieldGroup">
+                                {/* password*/}
+                                <div className="formField">
+                                    <label htmlFor="password">Password</label>
+                                    <InputText
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        values={values.password}
+                                        onChange={handleChange}
+                                        placeholder="secure..."
+                                    />
+                                    <ErrorMessage
+                                        name="password"
+                                        render={(msg) => (
+                                            <small
+                                                id="title-help"
+                                                className="p-error"
+                                            >
+                                                {msg}
+                                            </small>
+                                        )}
+                                    />
+                                </div>
                             </div>
-                            {/* password*/}
-                            <div className="formField">
-                                <label htmlFor="password">Password</label>
-                                <InputText
-                                    id="password"
-                                    type="password"
-                                    name="password"
-                                    values={values.password}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="password"
-                                    render={(msg) => (
-                                        <small
-                                            id="title-help"
-                                            className="p-error"
-                                        >
-                                            {msg}
-                                        </small>
-                                    )}
-                                />
-                            </div>
-                            {/* phone*/}
-                            <div className="formField">
-                                <label htmlFor="phone">Phone</label>
-                                <InputText
-                                    id="phone"
-                                    type="text"
-                                    name="phone"
-                                    values={values.phone}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="phone"
-                                    render={(msg) => (
-                                        <small
-                                            id="title-help"
-                                            className="p-error"
-                                        >
-                                            {msg}
-                                        </small>
-                                    )}
-                                />
-                            </div>
-                            {/* address*/}
-                            <div className="formField">
-                                <label htmlFor="address">Address</label>
-                                <InputText
-                                    id="address"
-                                    type="text"
-                                    name="address"
-                                    values={values.address}
-                                    onChange={handleChange}
-                                />
-                                <ErrorMessage
-                                    name="address"
-                                    render={(msg) => (
-                                        <small
-                                            id="title-help"
-                                            className="p-error"
-                                        >
-                                            {msg}
-                                        </small>
-                                    )}
-                                />
+                            <div className="formFieldGroup">
+                                {/* address*/}
+                                <div className="formField">
+                                    <label htmlFor="address">Address</label>
+                                    <InputTextarea
+                                        id="address"
+                                        type="text"
+                                        name="address"
+                                        values={values.address}
+                                        onChange={handleChange}
+                                        placeholder="123 Main St..."
+                                    />
+                                    <ErrorMessage
+                                        name="address"
+                                        render={(msg) => (
+                                            <small
+                                                id="title-help"
+                                                className="p-error"
+                                            >
+                                                {msg}
+                                            </small>
+                                        )}
+                                    />
+                                </div>
                             </div>
                             <div className="buttons">
                                 <Button type="submit" label="Register" />
-                                <Button
-                                    type="button"
-                                    label="Back to Login"
-                                    onClick={() => navigate("/login")}
-                                />
                             </div>
                         </Form>
                     )}
@@ -187,6 +199,11 @@ const RegisterPage = () => {
                 {success && <div className="success">{success}</div>}
                 {error && <div className="error">{error}</div>}
             </div>
+            <Button
+                type="button"
+                label="Back to Login"
+                onClick={() => navigate("/login")}
+            />
         </div>
     );
 };
--- frontend/src/schemas.js
+++ frontend/src/schemas.js
@@ -9,7 +9,13 @@ export const registrationSchema = Yup.object().shape({
     first_name: Yup.string().required("First name is required"),
     last_name: Yup.string().required("Last name is required"),
     email: Yup.string().email("Invalid email").required("Email is required"),
-    password: Yup.string().required("Password is required"),
+    password: Yup.string()
+        .required("Password is required")
+        .min(8, "Password must be at least 8 characters long")
+        .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+            "Password must contain a lowercase, uppercase, number, and special symbol"
+        ),
     user_type: Yup.string().required("User type is required"),
     phone: Yup.string().required("Phone is required"),
     address: Yup.string().required("Address is required"),
