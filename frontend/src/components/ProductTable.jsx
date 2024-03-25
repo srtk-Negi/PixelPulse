7070ea67157ad27d9c494fdaf2e460470e04d34c
Sarthak Negi <srnegi141@gmail.com>
2024-03-25 17:11:27 -0500

    products table design change

--- frontend/src/components/ProductTable.jsx
+++ frontend/src/components/ProductTable.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 import { Button } from "primereact/button";
+import { Tag } from "primereact/tag";
+import { useEffect } from "react";
 
 const ProductCard = ({ product }) => {
+    let severity;
+    let severityString;
+
+    const setSeverityData = () => {
+        let stock = product.items_in_stock;
+        if (stock > 50) {
+            severity = "success";
+            severityString = "In Stock";
+        } else if (stock > 10) {
+            severity = "warning";
+            severityString = "Low Stock";
+        } else {
+            severity = "danger";
+            severityString = "Out of Stock";
+        }
+    };
+
+    setSeverityData();
+
     return (
         <div className="productCard">
             <div className="productImg">
                 <img src={product.image_url} alt={product.title} />
             </div>
             <div className="details">
-                <h2>{product.title}</h2>
-                <p>{product.description}</p>
+                <div className="prodName">{product.name}</div>
+                <div className="category">{product.category}</div>
+                <div className="description">{product.description}</div>
+                <div className="priceStock">
+                    <div className="price">${product.price}</div>
+                    <Tag value={severityString} severity={severity} />
+                </div>
+                <Button label="Add to Cart" icon="pi pi-shopping-cart" />
             </div>
         </div>
     );
