2105a06205c031f43437c21687d136f5bee98afe
Sarthak Negi <srnegi141@gmail.com>
2024-04-03 22:14:22 -0500

    designed the cart items

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -8,18 +8,7 @@ const removeFromCart = (prod_id) => {
 };
 
 const ItemCard = ({ item }) => {
-    const header = (
-        <div className="prodImg">
-            <img
-                alt="Card"
-                src={item.image_url}
-                onError={(e) =>
-                    (e.target.src =
-                        "https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png")
-                }
-            />
-        </div>
-    );
+    const header = <img alt="Card" src={item.image_url} />;
     const footer = (
         <>
             <Button label={item.price} />
@@ -37,9 +26,9 @@ const ItemCard = ({ item }) => {
             <Card
                 title={item.prod_name}
                 subTitle={item.category}
-                style={{ width: "25rem" }}
-                footer={footer}
+                style={{ width: "25rem", height: "25rem" }}
                 header={header}
+                footer={footer}
             >
                 {item.description}
             </Card>
@@ -51,6 +40,7 @@ const Cart = () => {
     const [cart, setCart] = useState([]);
     const [total, setTotal] = useState(0);
     const token = localStorage.getItem("token");
+    const prices = [];
 
     useEffect(() => {
         const fetchCart = async () => {
@@ -66,11 +56,17 @@ const Cart = () => {
 
     return (
         <div className="cart">
-            <h1>Cart</h1>
+            <h1>
+                <i
+                    className="pi pi-shopping-cart"
+                    style={{ marginRight: "1rem", fontSize: "3rem" }}
+                />
+                Cart
+            </h1>
             <div className="cartItems">
-                {cart.map((item) => (
-                    <ItemCard item={item} key={item.prod_id} />
-                ))}
+                {cart.map((item) => {
+                    return <ItemCard item={item} key={item.prod_id} />;
+                })}
             </div>
             <div className="total">
                 <h2>Total: ${total}</h2>
